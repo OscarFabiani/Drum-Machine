e4601b36bdb30d33ea79806365d0f12b4803e3a5
OscarFabiani <spartan539@yahoo.com>
2019-02-24 14:58:13 -0800

    Added volume knob functionalty and styling, general styling,  and refactored code

--- script.js
+++ script.js
@@ -22,19 +22,98 @@ const c = document.getElementById('snare');
 //Other div elements
 const display = document.getElementById('display');
 const pad = document.getElementById('pad');
+const mySwitch = document.getElementById('switch');
+const knob = document.getElementById('knob');
+const volume = document.getElementById('volume');
 
 
-//Functions that reset+play the audio clip, sets the display innerText, and sets the background color to yellow(lit).
+
+function clearDisplay() {
+  //This function clears the display element. This function is neccessary to be passed into event listeners.
+  display.textContent = '';
+}
+
+function removeEndedListeners() {
+  //This function removes ended event listeners from all audio clips. This is to prevent the display from clearing while another audio clip is
+  //playing and is meant to be used in conjuction with adding an ended event listner to an individual clip when playback begins.
+  qClip.removeEventListener('ended', clearDisplay);
+  wClip.removeEventListener('ended', clearDisplay);
+  eClip.removeEventListener('ended', clearDisplay);
+  aClip.removeEventListener('ended', clearDisplay);
+  sClip.removeEventListener('ended', clearDisplay);
+  dClip.removeEventListener('ended', clearDisplay);
+  zClip.removeEventListener('ended', clearDisplay);
+  xClip.removeEventListener('ended', clearDisplay);
+  zClip.removeEventListener('ended', clearDisplay);
+}
+
+
+//Functions that reset+play the audio clip, playS the display innerText, set the background color to yellow(lit), remove event listners for
+//all clips ending to clear the display, and adds an event listener for the clip ending to clear the display.
 //NOTE: currentTime = 0 is necessary to allow the audio to restart when played before it ends.
-function setQ() {qClip.currentTime = 0; qClip.play(); display.innerText = 'Chord 1'; q.style.backgroundColor = 'rgb(250, 250, 200)';};
-function setW() {wClip.currentTime = 0; wClip.play(); display.innerText = 'Chord 2'; w.style.backgroundColor = 'rgb(250, 250, 200)';};
-function setE() {eClip.currentTime = 0; eClip.play(); display.innerText = 'Chord 3'; e.style.backgroundColor = 'rgb(250, 250, 200)';};
-function setA() {aClip.currentTime = 0; aClip.play(); display.innerText = 'Clap'; a.style.backgroundColor = 'rgb(250, 250, 200)';};
-function setS() {sClip.currentTime = 0; sClip.play(); display.innerText = 'Closed Hi-Hat'; s.style.backgroundColor = 'rgb(250, 250, 200)';};
-function setD() {dClip.currentTime = 0; dClip.play(); display.innerText = 'Open Hi-Hat'; d.style.backgroundColor = 'rgb(250, 250, 200)';};
-function setZ() {zClip.currentTime = 0; zClip.play(); display.innerText = 'Kick Drum'; z.style.backgroundColor = 'rgb(250, 250, 200)';};
-function setX() {xClip.currentTime = 0; xClip.play(); display.innerText = 'Side Stick'; x.style.backgroundColor = 'rgb(250, 250, 200)';};
-function setC() {cClip.currentTime = 0; cClip.play(); display.innerText = 'Snare Drum'; c.style.backgroundColor = 'rgb(250, 250, 200)';};
+function playQ() {
+  qClip.currentTime = 0;
+  qClip.play(); display.innerText = 'Chord 1';
+  q.style.backgroundColor = 'rgb(250, 250, 200)';
+  removeEndedListeners();
+  qClip.addEventListener('ended', clearDisplay);
+}
+function playW() {
+  wClip.currentTime = 0;
+  wClip.play(); display.innerText = 'Chord 2';
+  w.style.backgroundColor = 'rgb(250, 250, 200)';
+  removeEndedListeners();
+  wClip.addEventListener('ended', clearDisplay);
+}
+function playE() {
+  eClip.currentTime = 0;
+  eClip.play(); display.innerText = 'Chord 3';
+  e.style.backgroundColor = 'rgb(250, 250, 200)';
+  removeEndedListeners();
+  eClip.addEventListener('ended', clearDisplay);
+}
+function playA() {
+  aClip.currentTime = 0;
+  aClip.play(); display.innerText = 'Clap';
+  a.style.backgroundColor = 'rgb(250, 250, 200)';
+  removeEndedListeners();
+  aClip.addEventListener('ended', clearDisplay);
+}
+function playS() {
+  sClip.currentTime = 0;
+  sClip.play(); display.innerText = 'Closed Hi-Hat';
+  s.style.backgroundColor = 'rgb(250, 250, 200)';
+  removeEndedListeners();
+  sClip.addEventListener('ended', clearDisplay);
+}
+function playD() {
+  dClip.currentTime = 0; dClip.play();
+  display.innerText = 'Open Hi-Hat';
+  d.style.backgroundColor = 'rgb(250, 250, 200)';
+  removeEndedListeners();
+  dClip.addEventListener('ended', clearDisplay);
+}
+function playZ() {
+  zClip.currentTime = 0; zClip.play();
+  display.innerText = 'Kick Drum';
+  z.style.backgroundColor = 'rgb(250, 250, 200)';
+  removeEndedListeners();
+  zClip.addEventListener('ended', clearDisplay);}
+function playX() {
+  xClip.currentTime = 0;
+  xClip.play(); display.innerText = 'Side Stick';
+  x.style.backgroundColor = 'rgb(250, 250, 200)';
+  removeEndedListeners();
+  xClip.addEventListener('ended', clearDisplay);
+}
+function playC() {
+  cClip.currentTime = 0;
+  cClip.play(); display.innerText = 'Snare Drum';
+  c.style.backgroundColor = 'rgb(250, 250, 200)';
+  removeEndedListeners();
+  cClip.addEventListener('ended', clearDisplay);
+}
+
 
 //Pressed and depressed key functions
 //The pressed functions add the pressed class to the element while the depressed functions remove the pressed class and changes the background
@@ -58,7 +137,7 @@ function depressedX() {x.classList.remove('pressed'); x.style.backgroundColor = 
 function pressedC() {c.classList.add('pressed');}
 function depressedC() {c.classList.remove('pressed'); c.style.backgroundColor = 'rgb(202, 98, 60)'}
 
-//Adding the pressed class on mousedown and removing on mouseup or mouseout.
+//Adding listeners for adding the pressed class on mousedown and removing on mouseup or mouseout.
 q.addEventListener('mousedown', pressedQ);
 q.addEventListener('mouseup', depressedQ);
 q.addEventListener('mouseout', depressedQ);
@@ -163,34 +242,34 @@ function releaseKey(k) {
 }
 
 function onPush(k) {
-  //This function checks the key pressed and executes code that resets+plays the audio clip and sets the display innerText.
+  //This function checks the key pressed and executes code that replayS+plays the audio clip and playS the display innerText.
   switch(k.key.toLowerCase()) { //toLowerCase() is necessary here to include capital versions of keys(shift+key)
     case 'q':
-    setQ();
+    playQ();
     break;
     case 'w':
-    setW();
+    playW();
     break;
     case 'e':
-    setE();
+    playE();
     break;
     case 'a':
-    setA();
+    playA();
     break;
     case 's':
-    setS();
+    playS();
     break;
     case 'd':
-    setD();
+    playD();
     break;
     case 'z':
-    setZ();
+    playZ();
     break;
     case 'x':
-    setX();
+    playX();
     break;
     case 'c':
-    setC();
+    playC();
     break;
   }
   //document.removeEventListener('keydown', onPush); //This is necessary to stop onPush from repeating when a key is held down.
@@ -198,70 +277,105 @@ function onPush(k) {
 }
 
 
-//Power switch functions
-function addListeners() {
+//Volume
+
+function setVolume() {
+  qClip.volume = volume.value / 100;
+  wClip.volume = volume.value / 100;
+  eClip.volume = volume.value / 100;
+  aClip.volume = volume.value / 100;
+  sClip.volume = volume.value / 100;
+  dClip.volume = volume.value / 100;
+  zClip.volume = volume.value / 100;
+  xClip.volume = volume.value / 100;
+  cClip.volume = volume.value / 100;
+}
+
+function displayVolume() {
+  display.innerText = 'VOLUME: ' + volume.value;
+}
+function timeoutVolume() {
+  display.innerText = 'VOLUME: ' + volume.value;
+  setTimeout(() => display.innerText = '', 500);
+}
+
+//Adding volume slider listeners
+volume.addEventListener('change', setVolume);
+volume.addEventListener('input', setVolume);
+
+
+
+//Power switch
+
+function addOnListeners() {
   //Mouse event listeners
-  q.addEventListener('mousedown', setQ);
-  w.addEventListener('mousedown', setW);
-  e.addEventListener('mousedown', setE);
-  a.addEventListener('mousedown', setA);
-  s.addEventListener('mousedown', setS);
-  d.addEventListener('mousedown', setD);
-  z.addEventListener('mousedown', setZ);
-  x.addEventListener('mousedown', setX);
-  c.addEventListener('mousedown', setC);
+  q.addEventListener('mousedown', playQ);
+  w.addEventListener('mousedown', playW);
+  e.addEventListener('mousedown', playE);
+  a.addEventListener('mousedown', playA);
+  s.addEventListener('mousedown', playS);
+  d.addEventListener('mousedown', playD);
+  z.addEventListener('mousedown', playZ);
+  x.addEventListener('mousedown', playX);
+  c.addEventListener('mousedown', playC);
+
+  volume.addEventListener('change', timeoutVolume);
+  volume.addEventListener('input', displayVolume);
 
   //Keyboard event listeners
   document.addEventListener('keydown', onPush);
+
 }
 
-function removeListeners() {
+function removeOnListeners() {
   //Mouse event listeners
-  q.removeEventListener('mousedown', setQ);
-  w.removeEventListener('mousedown', setW);
-  e.removeEventListener('mousedown', setE);
-  a.removeEventListener('mousedown', setA);
-  s.removeEventListener('mousedown', setS);
-  d.removeEventListener('mousedown', setD);
-  z.removeEventListener('mousedown', setZ);
-  x.removeEventListener('mousedown', setX);
-  c.removeEventListener('mousedown', setC);
+  q.removeEventListener('mousedown', playQ);
+  w.removeEventListener('mousedown', playW);
+  e.removeEventListener('mousedown', playE);
+  a.removeEventListener('mousedown', playA);
+  s.removeEventListener('mousedown', playS);
+  d.removeEventListener('mousedown', playD);
+  z.removeEventListener('mousedown', playZ);
+  x.removeEventListener('mousedown', playX);
+  c.removeEventListener('mousedown', playC);
+
+  volume.removeEventListener('change', timeoutVolume);
+  volume.removeEventListener('input', displayVolume);
 
   //Keyboard event listeners
   document.removeEventListener('keydown', onPush);
 }
 
 
-//Power switch
-const mySwitch = document.getElementById('switch');
-const knob = document.getElementById('knob');
-
 function switchOff() {
-  knob.removeEventListener('click', switchOff);
   mySwitch.style.justifyContent = 'flex-start';
   knob.style.backgroundColor = 'red';
-  knob.addEventListener('click', switchOn);
   knob.textContent = "OFF";
   knob.style.color = 'black';
-  display.textContent = '';
-  removeListeners();
+  clearDisplay();
+  display.style.backgroundColor = 'rgb(192, 177, 146)';
+  knob.removeEventListener('click', switchOff);
+  knob.addEventListener('click', switchOn);
+  removeOnListeners();
 }
 
 function switchOn() {
-  knob.removeEventListener('click', switchOn);
   mySwitch.style.justifyContent = 'flex-end';
   knob.style.backgroundColor = 'blue';
-  knob.addEventListener('click', switchOff);
   knob.textContent = "ON";
   knob.style.color = 'white';
-  display.textContent = 'Welcome';
-  addListeners();
+  display.textContent = 'WELCOME';
+  display.style.backgroundColor = 'rgb(251, 243, 128)';
+  knob.removeEventListener('click', switchOn);
+  knob.addEventListener('click', switchOff);
+  addOnListeners();
 }
 
 switchOn();
 
 
 
-//REPLICATE VOLUME SLIDER
+//X
 
+//REFACTOR CODE
 //MAKE IT POSSIBLE TO PRESS MULTIPLE(AT LEAST 2) BUTTONS SIMULTANEOUSLY WHILE ALSO STOPPING AUDIO FROM BEING REPEATED WHEN KEY IS BEING HELD DOWN
